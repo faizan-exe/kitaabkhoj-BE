bb77bf7c69446a86cb14bcbe734a90c991dd940d
faizan-exe <faizanofficial120@gmail.com>
2023-12-17 22:27:16 +0500

    forgot to save

--- routes/index.ts
+++ routes/index.ts
@@ -4,13 +4,15 @@ import { Router } from 'express'
 import book from './book'
 import bookshop from './bookshop'
 import shopkeeper from './shopkeeper';
+import bookshopcatalog from './bookshopcatalog';
 
 const router = Router()
 
 
 router.use('/book', book);
 router.use('/bookshop', bookshop);
-router.use('/shopkeeper', shopkeeper)
+router.use('/shopkeeper', shopkeeper);
+router.use('/bookshopcatalog', bookshopcatalog);
 
 
 export default router
